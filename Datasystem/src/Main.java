98f3778c36dec6588fa3de8ab366cc659fcd1b8d
daeHui77 <103250672+daeHui77@users.noreply.github.com>
2022-05-16 23:26:42 +0900

    pass

---
+++ Datasystem/src/Main.java
@@ -0,0 +1,31 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame{
+    private static String id="";
+    //private InfoDialog settingScreen;
+    private static String title ="database";
+    //static JPanel panel;
+    private final static int WINDOW_HEIGHT = 720;
+    private final static int WINDOW_WIDTH = 1280;
+    public Main(String id){
+        super("Screen");
+        setTitle(title);
+        // setContentPane(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+        requestFocusInWindow();
+        setVisible(true);
+        Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation((windowSize.width - WINDOW_WIDTH) / 2,
+                (windowSize.height - WINDOW_HEIGHT) / 2);
+    }
+    public static void main(String[]args){
+        new Main("");
+    }
+
+}
