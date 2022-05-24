0b8d8ec2f9a570b7bcce8fa076c4bd707f2aeb4b
daeHui77 <103250672+daeHui77@users.noreply.github.com>
2022-05-24 19:37:04 +0900

    수정

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main extends JFrame{
     private static String title ="database";
     //static JPanel panel;
     private final static int WINDOW_HEIGHT = 720;
-    private final static int WINDOW_WIDTH = 1300;
+    private final static int WINDOW_WIDTH = 1280;
     public static void main(String[]args){
         new GlobalGUI("database");
         //new Main("");
