6b3b826171c6ecb2288292f6231b8ced778aa1ef
daeHui77 <103250672+daeHui77@users.noreply.github.com>
2022-05-30 01:22:38 +0900

    Frame

--- src/global/food_Dialog.java
+++ src/global/food_Dialog.java
@@ -8,16 +8,17 @@ public class food_Dialog extends JFrame {
     {
         setTitle("나만의 식단");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container c = getContentPane();
-        c.setLayout(null);
+        JFrame frame = new JFrame();
+        frame.setLayout(null);
+
 
         Scrollbar scr1 = new Scrollbar();
         scr1.setBounds(500, 400, 30, 200);
 
-        add(scr1);
+        frame.add(scr1);
 
-        setSize(1280,720);
-        setVisible(true);
+        frame.setSize(1280,720);
+        frame.setVisible(true);
     }
     public static void main(String [] args)
     {
