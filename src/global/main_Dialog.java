ab32cf323d96c9af28e52e68953a85c0232f8300
잼민 <79719602+gun2475@users.noreply.github.com>
2022-05-30 01:22:46 +0900

    main

--- src/global/main_Dialog.java
+++ src/global/main_Dialog.java
@@ -2,13 +2,23 @@ package global;
 
 import javax.swing.*;
 import Database.*;
-public class main_Dialog extends JFrame {
+
+import java.awt.*;
+
+public class main_Dialog extends JDialog {
     static JPanel mainPanel = new JPanel();
     private DB_Connect dbCon = new DB_Connect();
+    final static int WINDOW_HEIGHT = 720;
+    final static int WINDOW_WIDTH = 1280;
     main_Dialog(){
         dbCon.connect();
         setContentPane(mainPanel);
-        mainPanel.setSize(1280,720);
+        mainPanel.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
         mainPanel.setVisible(false);
+        setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+        setVisible(false);
+        Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation((windowSize.width - WINDOW_WIDTH) / 2,
+                (windowSize.height - WINDOW_HEIGHT) / 2);
     }
 }
