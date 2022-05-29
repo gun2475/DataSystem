71a33d52ef972db84e8074a029bc1d0698d77d04
daeHui77 <103250672+daeHui77@users.noreply.github.com>
2022-05-30 01:02:30 +0900

    test

--- src/global/food_Dialog.java
+++ src/global/food_Dialog.java
@@ -1,4 +1,20 @@
 package global;
+import java.awt.*;
+import javax.swing.*;
 
-public class food_Dialog {
+public class food_Dialog extends JFrame {
+    public food_Dialog()
+    {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = getContentPane();
+        c.setLayout(null);
+        setVisible(true);
+    }
+    public static void main(Sring [] args)
+    {
+        food_Dialog = new food_Dialog();
+    }
+
 }
+
+
