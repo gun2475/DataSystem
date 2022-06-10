d3add51af406411bbbf2881e3515edd516bb050a
znight1020 <104980470+znight1020@users.noreply.github.com>
2022-06-11 01:05:01 +0900

    디자인(로그인화면, 메뉴화면)

--- src/global/main_Dialog.java
+++ src/global/main_Dialog.java
@@ -22,42 +22,58 @@ public class main_Dialog extends JDialog {
         setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
         Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation((windowSize.width - WINDOW_WIDTH) / 2, (windowSize.height - WINDOW_HEIGHT) / 2);
-        username.setText(name+"님 반갑습니다.");
-        Font f = new Font("돋움체",Font.CENTER_BASELINE,20);
-        username.setForeground(new Color(178,102,255));
+        username.setText(name+"님");
+        Font f = new Font("Gothic",Font.CENTER_BASELINE,20);
+        username.setForeground(Color.GRAY);
         username.setFont(f);
-        username.setBounds(10,10,400,20);
+        username.setBounds(150,18,400,20);
         username.setVisible(true);
         mainPanel.add(username);
-        ImageIcon profileChange_png = new ImageIcon("src/asset/profileChange.png"); //이미지파일 크기 조정하는 하드코딩
-        Image img = profileChange_png.getImage();
-        Image changeImg = img.getScaledInstance(100,100,Image.SCALE_SMOOTH);
+
+        ImageIcon profileChange_jpg = new ImageIcon("src/asset/set.jpg"); //이미지파일 크기 조정하는 하드코딩
+        Image img = profileChange_jpg.getImage();
+        Image changeImg = img.getScaledInstance(70,70,Image.SCALE_SMOOTH);
         ImageIcon change_png = new ImageIcon(changeImg);
         JButton profileChange = new JButton(change_png);
-
         profileChange.setVisible(true);
-        profileChange.setBounds(380,10,100,100);
-
+        profileChange.setBounds(320,10,70,70);
         profileChange.setBorderPainted(false); //버튼 투명하게 만들기
         profileChange.setContentAreaFilled(false);
         mainPanel.add(profileChange);
-        ImageIcon button_1_png = new ImageIcon("src/asset/button_1.png");
-        Image img_1 = button_1_png.getImage();
-        Image changeImg_1 = img_1.getScaledInstance(300,100,Image.SCALE_SMOOTH);
-        ImageIcon change_png_1 = new ImageIcon(changeImg_1);
-        JButton food = new JButton(change_png_1);
-        food.setBounds(90,200,300,100);
-        food.setVisible(true);
-        food.setBorderPainted(false);
-        food.setContentAreaFilled(false);
-        mainPanel.add(food);
-
         profileChange.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 new UserInfo_Dialog(id);
             }
         });
+
+        ImageIcon exit_p = new ImageIcon("src/asset/exit.jpg");
+        Image exit_img = exit_p.getImage();
+        Image exitImg = exit_img.getScaledInstance(70,70,Image.SCALE_SMOOTH);
+        ImageIcon exit_jpg = new ImageIcon(exitImg);
+        JButton exit = new JButton(exit_jpg);
+        exit.setVisible(true);
+        exit.setBounds(400,10,70,70);
+        exit.setBorderPainted(false); //버튼 투명하게 만들기
+        exit.setContentAreaFilled(false);
+        mainPanel.add(exit);
+        exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        ImageIcon button_1_jpg = new ImageIcon("src/asset/식단.jpg");
+        Image img_1 = button_1_jpg.getImage();
+        Image changeImg_1 = img_1.getScaledInstance(262,213,Image.SCALE_SMOOTH);
+        ImageIcon change_png_1 = new ImageIcon(changeImg_1);
+        JButton food = new JButton(change_png_1);
+        food.setBounds(119,80,262,213);
+        food.setVisible(true);
+        food.setBorderPainted(false);
+        food.setContentAreaFilled(false);
+        mainPanel.add(food);
         food.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -65,13 +81,12 @@ public class main_Dialog extends JDialog {
             }
         });
 
-        ImageIcon button_2_png = new ImageIcon("src/asset/button_2.png");
-        Image img_2 = button_2_png.getImage();
-        Image changeImg_2 = img_2.getScaledInstance(300,100,Image.SCALE_SMOOTH);
+        ImageIcon button2_jpg = new ImageIcon("src/asset/graph.jpg");
+        Image img_2 = button2_jpg.getImage();
+        Image changeImg_2 = img_2.getScaledInstance(267,226,Image.SCALE_SMOOTH);
         ImageIcon change_png_2 = new ImageIcon(changeImg_2);
         JButton graph = new JButton(change_png_2);
-
-        graph.setBounds(90,350,300,100);
+        graph.setBounds(117,280,267,226);
         graph.setVisible(true);
         graph.setBorderPainted(false);
         graph.setContentAreaFilled(false);
@@ -81,13 +96,12 @@ public class main_Dialog extends JDialog {
             public void actionPerformed(ActionEvent e) { new graph_dialog(id);}
         });
 
-        ImageIcon button_3_png = new ImageIcon("src/asset/button_3.png");
-        Image img_3 = button_3_png.getImage();
-        Image changeImg_3 = img_3.getScaledInstance(300,100,Image.SCALE_SMOOTH);
+        ImageIcon button3_jpg = new ImageIcon("src/asset/made.jpg");
+        Image img_3 = button3_jpg.getImage();
+        Image changeImg_3 = img_3.getScaledInstance(172,182,Image.SCALE_SMOOTH);
         ImageIcon change_png_3 = new ImageIcon(changeImg_3);
         JButton made = new JButton(change_png_3);
-
-        made.setBounds(90,500,300,100);
+        made.setBounds(164,490,172,182);
         made.setVisible(true);
         made.setBorderPainted(false);
         made.setContentAreaFilled(false);
